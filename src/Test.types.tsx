21d8f15ae8fddac446a02d6108888b28fd94de72
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2021-04-08 15:19:04 +0100

    Initial commit

---
+++ src/Test.types.tsx
@@ -0,0 +1,12 @@
+export interface CardCompProps {
+  image: string
+  title: string
+  categories: Array<string>
+  description: string
+  url: string
+  cta?: string
+}
+
+export interface ImageProps {
+  layout: string
+}
