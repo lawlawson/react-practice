88a0c6013fa790812e1710bd4d36935f707ec23c
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2021-04-28 00:15:47 +0100

    Added latest changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import People from "./components/People";
-import Todo from './components/Todo';
+<<<<<<< Updated upstream
+=======
+import TodoComponent from './components/Todo';
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <People />
-      <Todo todos={[{id: 1, name: 'Lawrence'}, {id: 2, name: 'Manpreet'}]}/>
+=======
+      <TodoComponent
+        todos={[{id: 1, name: 'Lawrence'}, {id: 2, name: 'Manpreet'}]}
+        author={{ name: 'JK', numberOfBooks: 3}}
+      />
+>>>>>>> Stashed changes
     </div>
   );
 }
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -5,16 +5,36 @@ interface todo {
   name: string
 }
 
-interface todos {
+interface todosProps {
   todos: todo[]
+  author: author
+  // numberOfBooks: number
 }
 
-const Todo: React.FC<todos> = ({todos}) => {
-  return <ul>
-    {todos.map((todo) => (
-      <li>{todo.name}</li>
-    ))}
-    </ul>;
+interface author {
+  name: string
+  numberOfBooks: number
+}
+// const Todo: React.FC<todos> = ({todos}) => {
+//   return <ul>
+//     {todos.map((todo) => (
+//       <li>{todo.name}</li>
+//     ))}
+//     </ul>;
+// }
+
+const TodoComponent: React.FC<todosProps> = (props) => {
+  console.log(props);
+
+  return (
+  <div>
+    <ul>
+      {props.todos.map((todo) => (<li>{todo.name}</li>))}
+    </ul>
+      <p>{props.author.numberOfBooks}</p>
+      <p>{props.author.name}</p>
+  </div>
+  );
 }
 
-export default Todo;
+export default TodoComponent;
